ff12beaa63350e7c65d5387d05b98fb4506073fb
wmustaqim19 <w.mustaqim1019@gmail.com>
2025-12-23 13:25:01 +0700

    update node path

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mysql = require('mysql');
 const app = express()
-const path = require('path')
+const path = require('node:path')
 require('dotenv').config();
 
 // Import Middleware
